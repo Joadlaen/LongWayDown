bf096b14284c93f2ff7987382318b91f63590fae
Joadlaen <jackcullenmurphy1999@gmail.com>
2026-03-05 17:15:03 +0000

    added heightmap for variation in the level and for movement logic

--- LongWayDown/Assets/scripts/GridSet.cs
+++ LongWayDown/Assets/scripts/GridSet.cs
@@ -4,19 +4,32 @@ public class GridSet : MonoBehaviour
 {
 
     public GameObject blankSquare;
+    public GameObject Playerchar;
+
+    Vector3 CharPOS;
+
     public int gridHorSize = 20;
     public int gridVertSize = 30;
-    
+
+
+    float[,] heightmap;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Playerchar.transform.position = CharPOS;
+        float[,] heightmap = new float[gridHorSize, gridVertSize];
         for (int i = 0; i < gridHorSize; i++)
         {
             for (int j = 0; j < gridVertSize; j++)
             {
-                Instantiate(blankSquare, new Vector3(i, 0, j), Quaternion.identity);
+                Instantiate(blankSquare, new Vector3(i, heightmap[i,j], j), Quaternion.identity);
             }
         }
+
+        CharPOS.x = 10;
+        CharPOS.y = 1.5f + heightmap[(int)CharPOS.x,(int)CharPOS.y];
+        CharPOS.z = 15;
     }
 
     // Update is called once per frame
